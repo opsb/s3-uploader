bf9876c67094b0b269adeb8e193774f37a57d5f4
opsb <oliver@opsb.co.uk>
2010-08-24 12:20:03 +0100

    Non urls are now ignored

--- server.rb
+++ server.rb
@@ -3,10 +3,11 @@ require 'sinatra'
 require 'aws/s3'
 require 'digest'
 require 'mime/types'
+require 'uri'
 
 AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['MSD_AMAZON_S3_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['MSD_AMAZON_SECRET_ACCESS_KEY']
+  :access_key_id     => ENV['AMAZON_S3_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AMAZON_S3_SECRET_ACCESS_KEY']
 )
 
 get '/' do
@@ -14,27 +15,31 @@ get '/' do
 end
 
 post '/' do
-  filename = params['image'].gsub(/\?.*$/, '')
-  type = MIME::Types.type_for(filename).first
-  path = 'newsletters_demo/' + Digest::SHA1.hexdigest(params['image']) + '.' + type.extensions.first
-  AWS::S3::S3Object.store(path, 
-                 open(params['image']), 
-                 ENV['MSD_IMAGES_BUCKET'],
-                 :access => :public_read,
-                 :content_type => type.to_s)
-  url = "http://#{ENV['MSD_IMAGES_BUCKET']}.s3.amazonaws.com/" + path
-  redirect url
+  if params['url'] =~ URI.regexp
+    filename = params['url'].gsub(/\?.*$/, '')
+    type = MIME::Types.type_for(filename).first
+    path = ENV['AMAZON_S3_PATH'] + Digest::SHA1.hexdigest(params['url']) + '.' + type.extensions.first
+    AWS::S3::S3Object.store(path,
+                   open(params['url']), 
+                   ENV['AMAZON_S3_BUCKET'],
+                   :access => :public_read,
+                   :content_type => type.to_s)
+    url = "http://#{ENV['AMAZON_S3_BUCKET']}.s3.amazonaws.com/" + path
+    redirect url
+  else
+    redirect '/'
+  end
 end
 
 __END__
 
 @@ index
 
-<h1>Image loader</h1>
+<h1>Amazon uploader</h1>
 
 <form method="post">
-  <label>Image</label>
-  <input name="image" type="text" />
+  <label>File</label>
+  <input name="url" type="text" />
   <input type="submit" value="Load" />
 </form>
 
